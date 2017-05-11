5aad0443ccdc94e76cd695776bd197257f6c3339
jonasdegelo <jonas.degelo@gmail.com>
2017-05-11 14:01:30 +0200

    test commit

---
+++ testingground/registration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="hompage.css">
+	<title>Login</title>
+</head>
+
+<body> <!--style="background-color:black;"-->>
+
+<?php
+	echo "<p>Hello World!</p>";
+?>
+</body>
+</html>
