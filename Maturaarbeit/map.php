553c986a7731f5000c231682c7dbcf8fea5eac26
jonasdegelo <jonas.degelo@gmail.com>
2017-06-04 21:53:32 +0200

    parse

--- Maturaarbeit/map.php
+++ Maturaarbeit/map.php
@@ -25,7 +25,7 @@ include 'db.php';
 	</script>
 	
 	<?php //gpx test;
-	use phpGPX\phpGPX;
+	/*use phpGPX\phpGPX;
 	
 	$gpx = new phpGPX();
 		
@@ -35,13 +35,29 @@ include 'db.php';
 		
 		// Statistics for whole track
 		$track->stats->toArray();
-		print_r($track);
+		//print_r($track);
 		    
 		foreach ($track->segments as $segment)
 		{
 			// Statistics for segment of track
 			$segment->stats->toArray();
 			//print_r($segment);
+		}
+	}*/
+	?>
+	
+	<?php
+	$GPXParser = new GPXParser();
+	
+	$xml = "activities/gpx/example.gpx";
+	
+	$gpx = $GPXParser->parseXML($xml);
+	
+	foreach ($gpx->getTracks() as $track) {
+		foreach ($track->getTrackSegments() as $trackSegment) {
+			foreach ($trackSegment->getTrackPoints() as $waypoint) {
+				echo 'Lat: ' . $waypoint->getLatitude() . ', Lon:' . $waypoint->getLongitude();
+			}
 		}
 	}
 	?>
