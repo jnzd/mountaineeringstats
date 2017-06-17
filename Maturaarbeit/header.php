5bc67106a9c22c0bfe20f09bdd68dfa5925a84f4
jonasdegelo <jonas.degelo@gmail.com>
2017-06-17 14:05:03 +0200

    search bar+css optimization

--- Maturaarbeit/header.php
+++ Maturaarbeit/header.php
@@ -7,6 +7,7 @@
 	if(!isset($title)){
 		$title="Home";
 	}
+	//Fehlermeldungen leeren
 	if(!isset($_SESSION['message'])){
 		$_SESSION['message'] = "";
 	}
@@ -16,6 +17,7 @@
 <head>
 	<title><?php echo $title; ?></title>
 	<link href="style.css" rel="stylesheet" type="text/css">
+<!-- Google maps polyline meta -->
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 </head>
 
@@ -31,26 +33,39 @@
 		}
 
 		$profilepic = $row['pic_path'];
-		echo "<header>
-				<nav>
-					<ul>
-						<li><a href='map.php'>KARTE</a></li>
-						<li><a href='parse.php'>PARSER</a></li>
-						<li><a href='feed.php'>FEED</a></li>
-						<li><a href='index.php'>STARTSEITE</a></li>
-						<li><a href='upload.php'>HOCHLADEN</a></li>
-						<li><a href='profile.php'><img class='circle' src='".$profilepic."'height='24' width='24'></a></li>
-						<li><a href='includes/logout.inc.php'>ABMELDEN</a></li>
-					</ul>
-				</nav>
-			</header>";
+		//Header anzeigen
+		echo "
+			<header>
+				<div class='header-inner'>
+<!--Website logo -->
+					<a href='index.html' id='logo'></a>
+					<form class='search' action='search.inc.php' method='post'>
+						<input type='text' name='search' placeholder='Suchen'>
+					</form>
+					<nav>
+<!-- menu fuer mobiles design -->
+						<a href='#' id='menu-icon'></a>
+						<ul>
+<!-- Links im Header -->
+							<li><a href='map.php'>KARTE</a></li>
+							<li><a href='parse.php'>PARSER</a></li>
+							<li><a href='feed.php'>FEED</a></li>
+							<li><a href='index.php'>STARTSEITE</a></li>
+							<li><a href='upload.php'>HOCHLADEN</a></li>
+							<li><a href='profile.php'><img class='circle' src='".$profilepic."'height='24' width='24'></a></li>
+							<li><a href='includes/logout.inc.php'>ABMELDEN</a></li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		";
 	}else{
 		if($title != "Hi" && $title != "Willkommen"){
 			header("location: start.php");
 		}
 	}
 ?>
-
-<div id="body">
-
 <body>
+<form class="" action="index.html" method="post">
+
+</form>
