6ee2f325ee85482cdb5f413cadc7f8acb4d7b626
jonasdegelo <jonas.degelo@gmail.com>
2017-08-09 18:49:13 +0200

    username urls

--- Maturaarbeit/index.php
+++ Maturaarbeit/index.php
@@ -1,17 +1,33 @@
 <?php
-$title="Startseite";
-include 'header.php';
 include 'db.php';
-?>
-<h1>Willkommen</h1>
-<div id="feed">
-	
-	
-	<?php
-		//display news feed
-	?>
-</div>
+preg_match("/[^\/]+$/", $_SERVER['REQUEST_URI'], $matches);
+$last_word = $matches[0];
+$sql = "SELECT * FROM users WHERE username='$last_word'";
+$result = $conn->query($sql);
+$rownr = $result->num_rows;
 
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
+$title="mountaineeringstats";
+include 'header.php';
+
+if($rownr>0){
+	$row = $result->fetch_assoc();
+	include 'includes/publicProfile.inc.php';
+}else{
+	echo "
+	<h1>Willkommen</h1>
+	<div id='feed'>
+
+
+		<?php
+			//display news feed
+		?>
+	</div>
+	";
+}
+?>
 <?php include 'footer.php'; ?>
 
 </body>
