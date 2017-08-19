bcf7d0cbca559ef74f5fec47458bc707381d5653
jonasdegelo <jonas.degelo@gmail.com>
2017-08-19 11:01:20 +0200

    elevation chart

--- Maturaarbeit/includes/charts.inc.php
+++ Maturaarbeit/includes/charts.inc.php
@@ -7,15 +7,15 @@
 	console.log(<?php echo $elevation_js; ?>);
 	var elevation = <?php echo $elevation_js; ?>;
 	var ctx = document.getElementById("elevation").getContext('2d');
-	var myLineChart = new Chart(ctx, {
+	var elevationChart = new Chart(ctx, {
     type: 'line',
     data: {
-			data: elevation,
-			label: "elevation",
-			xAxisID: "Meter über Meer",
-			yAxisID: "Zeit",
-			backgroundColor: "#a8a8a8",
-			borderColor: "#919191"
+			datasets: [{
+				//label: "elevation",
+				backgroundColor: "#a8a8a8",
+				borderColor: "#919191",
+				data: elevation,
+			}]
 		},
     options: {
 			responsive: true,
@@ -26,6 +26,42 @@
 			}
 		}
 	});
+	/*var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+    },
+    options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+    }
+	});*/
 	/*var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
