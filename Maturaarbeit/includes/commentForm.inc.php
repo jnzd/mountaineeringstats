2929cd8b56dc85b11a3a15d6bf3a412ae865e595
jonasdegelo <jonas.degelo@gmail.com>
2017-08-29 17:55:35 +0200

    freshly posted comment displays

--- Maturaarbeit/includes/commentForm.inc.php
+++ Maturaarbeit/includes/commentForm.inc.php
@@ -18,11 +18,18 @@
         url = $form.attr( 'action' );
 
     /* Send the data using post with element id name and name2*/
-    var posting = $.post( url, { commentText: $('#commentText').val(), userID: '<?php echo $userID; ?>', actID: '<?php echo $actid; ?>'} );
+    var commentText = $('#commentText').val();
+    var username = "<?php 
+       $sql = "SELECT * FROM users WHERE id='$userID'";
+       $result = $conn->query($sql);
+       $row = $result->fetch_assoc();
+       echo $row['username'];
+    ?>";
+    var posting = $.post( url, { commentText, userID: '<?php echo $userID; ?>', actID: '<?php echo $actid; ?>'} );
 
     /* Alerts the results */
     posting.done(function( data ) {
-      $('#comments').html('<p>'+commentText+'</p>');
+      $('#comments').html('<div id="commentLine"><p class="commentName">'+username+': <p class="commentText">'+commentText+'</p></div>');
     });
   });
 </script>
