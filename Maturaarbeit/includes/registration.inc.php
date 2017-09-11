44fc955f3df70c17099d391e4fbb097b4406e21f
jonasdegelo <jonas.degelo@gmail.com>
2017-09-11 18:03:07 +0200

    random confirmation hash

--- Maturaarbeit/includes/registration.inc.php
+++ Maturaarbeit/includes/registration.inc.php
@@ -53,7 +53,32 @@ if($_POST['password'] != $_POST['confirmpassword']){
 		}
 #######################################################################################################################
 		//confirmation code
-		$confirm_code = md5($username);
+		function crypto_rand_secure($min, $max) {
+			$range = $max - $min;
+			if ($range < 0) return $min; // not so random...
+			$log = log($range, 2);
+			$bytes = (int) ($log / 8) + 1; // length in bytes
+			$bits = (int) $log + 1; // length in bits
+			$filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+			do {
+					$rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+					$rnd = $rnd & $filter; // discard irrelevant bits
+			} while ($rnd >= $range);
+			return $min + $rnd;
+		}
+
+		function getToken($length=32){
+			$token = "";
+			$codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			$codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+			$codeAlphabet.= "0123456789";
+			for($i=0;$i<$length;$i++){
+					$token .= $codeAlphabet[crypto_rand_secure(0,strlen($codeAlphabet))];
+			}
+			return $token;
+		}
+		$confirmSeed = getToken();
+		$confirm_code = md5($confirmSeed);
 		//safes code to db
 		$sql = "INSERT INTO users (username, email, password, confirm_code)	VALUES ('$username', '$email', '$password', '$confirm_code')";
 		$result = mysqli_query($conn, $sql);
