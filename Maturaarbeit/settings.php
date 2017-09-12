e9931dfd0328ac6015c4d8d2b58cff6f4badac50
jonasdegelo <jonas.degelo@gmail.com>
2017-09-12 20:47:49 +0200

    working on settings design

--- Maturaarbeit/settings.php
+++ Maturaarbeit/settings.php
@@ -16,7 +16,7 @@
 <div class="settings">
 	<h1 class="settingsTitle">Einstellungen</h1>
 	 <div class="settingsForm">
-		<a href="profile.php">Zurück</a>
+		<a class="settings" href="profile.php">Zurück</a>
 		<form id="settings" action="includes/settings.inc.php" method="post" enctype="multipart/form-data">
 			<input type="text" name="username" placeholder="Benutzername" value="<?php echo $username; ?>"><br>
 			<input type="email" name="email" placeholder="E-Mail" value="<?php echo $email; ?>"><br>
@@ -24,11 +24,6 @@
 			<input type="password" name="confirmpassword" placeholder="Bestätige neues Passwort"><br>
 			<input type="text" name="first" placeholder="Vorname" value="<?php echo $first; ?>"><br>
 			<input type="text" name="last" placeholder="Nachname" value="<?php echo $last; ?>"><br>
-			<!--<input type="text" name="ort" placeholder="Wohnort"><br>
-			<input type="number" name="plz" placeholder="PLZ"><br>
-			<input type="text" name="street" placeholder="Strasse"><br>
-			<input type="number" name="strnr" placeholder="Hausnummer"><br>
-			<input type="text" name="country" placeholder="Land"><br>-->
 			<select name="gender">
 				<option selected="selected" value="<?php echo $gender; ?>">Geschlecht</option>
 				<option value="male">männlich</option>
@@ -38,7 +33,7 @@
 			<input type="file" name="pic" placeholder="Profilbild"><br>
 			<input type="submit" name="safe" placeholder="speichern"><br><br>
 		</form>
-		<a id="settings" href="includes/delete.inc.php">Account löschen</a>
+		<a class="delete settings" href="includes/delete.inc.php">Account löschen</a>
 	</div>
 </div>
 <?php
