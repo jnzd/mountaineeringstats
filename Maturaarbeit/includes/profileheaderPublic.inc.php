7e6b142548a4321d82c05a29ef41de7cb11f74d7
jonasdegelo <jonas.degelo@gmail.com>
2017-09-12 20:15:10 +0200

    design|profile header

--- Maturaarbeit/includes/profileheaderPublic.inc.php
+++ Maturaarbeit/includes/profileheaderPublic.inc.php
@@ -1,16 +1,20 @@
 <div class="profileheader">
-  <a href="<?php echo $row['username'];?>"><img class="circle" src="<?php echo $row['pic_path'];?>" height="120" width="120"></a>
-  <?php
-    $userID = $row['id'];
-    echo "<h1 class='username'>".$publicUser."</h1>";
-    include 'includes/followerNumbers.inc.php';
-  ?>
-  <div class="followButton" id="followButton">
-    <?php
-      include 'followButton.inc.php';
-    ?>
+  <div class="profilepicture">
+    <a href="<?php echo $row['username'];?>"><img class="circle" src="<?php echo $row['pic_path'];?>" height="120" width="120"></a>
   </div>
-  <div class="bio">
+  <div class="profileInfo">
+    <?php
+      $userID = $row['id'];
+      echo "<h1 class='username'>".$publicUser."</h1>";
+      include 'includes/followerNumbers.inc.php';
+    ?>
+    <div class="followButton" id="followButton">
+      <?php
+        include 'followButton.inc.php';
+      ?>
+    </div>
+    <div class="bio">
 
+    </div>
   </div>
 </div>
