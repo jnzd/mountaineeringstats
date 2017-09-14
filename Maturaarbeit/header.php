1dac4c285a49417bba8829267fbc57c4730eee3a
jonasdegelo <jonas.degelo@gmail.com>
2017-09-14 21:11:03 +0200

    slightly responsive registration page

--- Maturaarbeit/header.php
+++ Maturaarbeit/header.php
@@ -17,7 +17,8 @@
 	<title><?php echo $title; ?></title>
 	<link href="style.css" rel="stylesheet" type="text/css">
 	<!-- Google maps polyline meta -->
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<!--<meta name="viewport" content="initial-scale=1.0, user-scalable=no">-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <?php
