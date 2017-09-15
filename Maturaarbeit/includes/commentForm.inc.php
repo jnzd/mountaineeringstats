402b5e43acdbe00dbb4bd65fe7bea9ebfa57ed1b
jonasdegelo <jonas.degelo@gmail.com>
2017-09-15 21:36:16 +0200

    ajax comments

--- Maturaarbeit/includes/commentForm.inc.php
+++ Maturaarbeit/includes/commentForm.inc.php
@@ -1,11 +1,6 @@
 <div class="commentForm<?php echo $actid; ?>">
   <textarea></textarea>
   <button type="button" onclick="postComment()">Kommentieren</button>
-  <form class="commentForm" action="includes/comment.inc.php" method="post" enctype="multipart/form-data">
-    <input type="text" id="commentText" name="commentText" placeholder="Kommentar hinzufügen" autocomplete="off"><br>
-    <input type="hidden" id="actID" name="actID" value="<?php echo $actid;?>">
-    <input type="hidden" id="url" name="url" value="<?php echo $_SERVER['REQUEST_URI'];?>">
-  </form>
 </div>
 <script>
   function postComment(){
@@ -14,10 +9,17 @@
       url:'includes/comment.inc.php',
       datatype: 'json',
       data: {
-        id: '<?php echo $actid; ?>',
-        actid: '<?php echo $_SERVER['REQUEST_URI']; ?>',
-        commentText: 
-        actid: '<?php echo $username; ?>'
+        id: '<?php echo $_SESSION['id']; ?>',
+        actid: '<?php echo $actid; ?>',
+        //commentText: 
+        username: '<?php 
+          $id=$_SESSION['id'];
+          $sql="SELECT * FROM users WHERE id='$id'";
+          $result=$conn->query($sql);
+          $rowUser=$result->fetch_assoc();
+          $usernam=$rowUser['username'];
+          echo $username; 
+        ?>',
       },
       complete: function (response) {
         $("#comments").append("<div id='commentLine'><p class='comment'><b>"+username+"</b>"+commentText);
