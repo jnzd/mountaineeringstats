c5e679e238c27bf744d9e48a44e3bbb4ee680f5f
jonasdegelo <jonas.degelo@gmail.com>
2017-09-15 21:54:09 +0200

    comments...

--- Maturaarbeit/includes/comment.inc.php
+++ Maturaarbeit/includes/comment.inc.php
@@ -2,10 +2,8 @@
   session_start();
 	include '../db.php';
 	$id = $_SESSION['id'];
-  $actID = $_POST['actID'];
+  $actid = $_POST['actid'];
   $commentText = $_POST['commentText'];
-	$sql = "INSERT INTO comments (userID, actID, commentText)	VALUES ('$id', '$actID', '$commentText')";
+	$sql = "INSERT INTO comments (userID, actID, commentText)	VALUES ('$id', '$actid', '$commentText')";
 	$result = $conn->query($sql);
-	$url = $_POST['url'];
-	//header("location: ..".$url);
 ?>
--- Maturaarbeit/includes/commentForm.inc.php
+++ Maturaarbeit/includes/commentForm.inc.php
@@ -4,25 +4,28 @@
 </div>
 <script>
   function postComment(){
+    var username='<?php 
+      $id=$_SESSION['id'];
+      $sql="SELECT * FROM users WHERE id='$id'";
+      $result=$conn->query($sql);
+      $rowUser=$result->fetch_assoc();
+      $usernam=$rowUser['username'];
+      echo $username; 
+    ?>';
+    var commentText='test';
+    var actid='<?php echo $actid; ?>';
     $.ajax({
       type:'post',
       url:'includes/comment.inc.php',
       datatype: 'json',
       data: {
         id: '<?php echo $_SESSION['id']; ?>',
-        actid: '<?php echo $actid; ?>',
-        //commentText: 
-        username: '<?php 
-          $id=$_SESSION['id'];
-          $sql="SELECT * FROM users WHERE id='$id'";
-          $result=$conn->query($sql);
-          $rowUser=$result->fetch_assoc();
-          $usernam=$rowUser['username'];
-          echo $username; 
-        ?>',
+        actid: actid,
+        commentText: commentText,
+        username: username
       },
       complete: function (response) {
-        $("#comments").append("<div id='commentLine'><p class='comment'><b>"+username+"</b>"+commentText);
+        $("#comments"+actid).append("<div id='commentLine'><p class='comment'><b>"+username+"</b>"+commentText);
       },
     });
   }
