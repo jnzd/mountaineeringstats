129b154da62f51b1386388a8325514761152fdc1
jonasdegelo <jonas.degelo@gmail.com>
2017-09-15 21:29:14 +0200

    ajax comments

--- Maturaarbeit/includes/comment.inc.php
+++ Maturaarbeit/includes/comment.inc.php
@@ -1,11 +1,10 @@
 <?php
   session_start();
 	include '../db.php';
-	$userID = $_SESSION['id'];
+	$id = $_SESSION['id'];
   $actID = $_POST['actID'];
   $commentText = $_POST['commentText'];
-  
-	$sql = "INSERT INTO comments (userID, actID, text)	VALUES ('$userID', '$actID', '$commentText')";
+	$sql = "INSERT INTO comments (userID, actID, commentText)	VALUES ('$id', '$actID', '$commentText')";
 	$result = $conn->query($sql);
 	$url = $_POST['url'];
 	//header("location: ..".$url);
--- Maturaarbeit/includes/commentForm.inc.php
+++ Maturaarbeit/includes/commentForm.inc.php
@@ -1,7 +1,27 @@
-<div class="commentForm<?php //echo $actid; ?>">
+<div class="commentForm<?php echo $actid; ?>">
+  <textarea></textarea>
+  <button type="button" onclick="postComment()">Kommentieren</button>
   <form class="commentForm" action="includes/comment.inc.php" method="post" enctype="multipart/form-data">
     <input type="text" id="commentText" name="commentText" placeholder="Kommentar hinzufügen" autocomplete="off"><br>
     <input type="hidden" id="actID" name="actID" value="<?php echo $actid;?>">
     <input type="hidden" id="url" name="url" value="<?php echo $_SERVER['REQUEST_URI'];?>">
   </form>
 </div>
+<script>
+  function postComment(){
+    $.ajax({
+      type:'post',
+      url:'includes/comment.inc.php',
+      datatype: 'json',
+      data: {
+        id: '<?php echo $actid; ?>',
+        actid: '<?php echo $_SERVER['REQUEST_URI']; ?>',
+        commentText: 
+        actid: '<?php echo $username; ?>'
+      },
+      complete: function (response) {
+        $("#comments").append("<div id='commentLine'><p class='comment'><b>"+username+"</b>"+commentText);
+      },
+    });
+  }
+</script>
