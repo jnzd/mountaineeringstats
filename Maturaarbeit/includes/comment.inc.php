2d8edaa8a9273ae06dab9417e124e67a15456bfe
jonasdegelo <jonas.degelo@gmail.com>
2017-09-15 21:00:16 +0200

    cemments

--- Maturaarbeit/includes/comment.inc.php
+++ Maturaarbeit/includes/comment.inc.php
@@ -8,5 +8,5 @@
 	$sql = "INSERT INTO comments (userID, actID, text)	VALUES ('$userID', '$actID', '$commentText')";
 	$result = $conn->query($sql);
 	$url = $_POST['url'];
-	header("location: ..".$url);
+	//header("location: ..".$url);
 ?>
