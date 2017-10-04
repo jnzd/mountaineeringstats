609d3299c1b245a299131bee372b939ca7f413d4
jonasdegelo <jonas.degelo@gmail.com>
2017-10-04 19:20:58 +0200

    writing to notifications table

--- Maturaarbeit/includes/comment.inc.php
+++ Maturaarbeit/includes/comment.inc.php
@@ -9,6 +9,11 @@
   $commentText = $conn->real_escape_string($_POST['commentText']);
 	$sql = "INSERT INTO comments (userID, actID, commentText)	VALUES ('$id', '$actid', '$commentText')";
 	$result = $conn->query($sql);
-	//printf ("New Record has id %d.\n", $conn->insert_id);
-	echo json_encode($conn->insert_id);
+	$sql = "SELECT * FROM activities where id='$actid'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$recievingid = $row['user_id'];
+	$link = "activity.php?name=".$row['actPath'];
+	$sql = "INSERT INTO notifications (receivingID, sendingID, type, link) VALUES ('$recievingid','$id','comment','$link')";
+	$result = $conn->query($sql);
 ?>
--- Maturaarbeit/includes/like.inc.php
+++ Maturaarbeit/includes/like.inc.php
@@ -9,4 +9,11 @@
   $userID00actID = $userID."00".$actID;
 	$sql = "INSERT INTO likes (userID, actID, userID00actID) VALUES ('$userID', '$actID', '$userID00actID')";
 	$result = $conn->query($sql);
+	$sql = "SELECT * FROM activities where id='$actID'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$recievingid = $row['user_id'];
+	$link = "activity.php?name=".$row['actPath'];
+	$sql = "INSERT INTO notifications (receivingID, sendingID, type, link) VALUES ('$recievingid','$id','like','$link')";
+	$result = $conn->query($sql);
 ?>
