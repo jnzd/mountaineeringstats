8b2a98a6094135b2578e4ec5c56f63349e30263d
jonasdegelo <jonas.degelo@gmail.com>
2017-10-04 21:05:06 +0200

    working on notifications

--- Maturaarbeit/header.php
+++ Maturaarbeit/header.php
@@ -76,6 +76,44 @@
 									echo "<button onclick=noNotifications() class='noNotifications headerButton'></button>";
 								}
 							?>
+							<!--<div class="dropdown-content" id="dropdown-content">-->
+								<?php
+									$sql = "SELECT * FROM notifications WHERE receivingID='$id'";
+									$result = $conn->query($sql);
+									$rownr = $result->num_rows;
+									if($rownr>0){
+										$rows = resultToArray($result);
+										foreach($rows as $row){
+											$type = $row['type'];
+											if($type == "follower"){
+												$followingid = $row['sendingID'];
+												$sql = "SELECT * FROM users WHERE id='$followingid'";
+												$result = $conn->query($sql);
+												$row2 = $result->fetch_assoc();
+												$follower = $row2['username'];
+												$link = $row['link'];
+												echo "<a href='".$link."'>".$follower." folgt dir jetzt</a>";
+											}else if($type == "comment"){
+												$commentingid = $row['sendingID'];
+												$sql = "SELECT * FROM users WHERE id='$commentingid'";
+												$result = $conn->query($sql);
+												$row2 = $result->fetch_assoc();
+												$commenter = $row2['username'];
+												$link = $row['link'];
+												echo "<a href='".$link."'>".$commenter." hat unter deinem Beitrag kommentiert</a>";
+											}else{
+												$likingid = $row['sendingID'];
+												$sql = "SELECT * FROM users WHERE id='$likingid'";
+												$result = $conn->query($sql);
+												$row2 = $result->fetch_assoc();
+												$liker = $row2['username'];
+												$link = $row['link'];
+												echo "<a href='".$link."'>".$liker." gefällt dein Beitrag</a>";
+											}
+										}
+									}
+								?>
+							<!--</div>-->
 						</li>
 						<?php include 'includes/notifications.inc.php';?>
 						<!-- profile dropdown -->
