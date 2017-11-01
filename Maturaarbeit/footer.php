038849b9634f58e6a25e1886a7fbfc288f7ca43b
jonasdegelo <jonas.degelo@gmail.com>
2017-11-01 18:01:07 +0100

    footer format

--- Maturaarbeit/footer.php
+++ Maturaarbeit/footer.php
@@ -1,9 +1,9 @@
 	</div>
 	<footer>
-		<?php
-
-		?>
-		<div class="iconCredit">Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>, <a href="http://www.freepik.com" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		<div class="footer">
+			<div class='contact'><p>Maturaarbeit Jonas Degelo <a class='footerEmail' href='mailto:jonas.degelo@gmail.com'>jonas.degelo@gmail.com</a> 6060 Sarnen, 2017</p>
+			<div class="iconCredit">Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>, <a href="http://www.freepik.com" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		</div>
 	</footer>
 </div>
 </body>
