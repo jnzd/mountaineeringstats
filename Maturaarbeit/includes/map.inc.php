bf4be8f1d73f88c06ac92c646c6613b6e2eb3c64
jonasdegelo <jonas.degelo@gmail.com>
2017-11-03 21:14:57 +0100

    rm maturaarbeit

--- Maturaarbeit/includes/map.inc.php
+++
@@ -1,7 +0,0 @@
-<div id="map"></div>
-<script>
-  //set javascript arrays
-  var lat = <?php echo $lat_js; ?>;
-  var lng = <?php echo $long_js; ?>;
-</script>
-<script src="javascript/map.js"></script>
--- Maturaarbeit/javascript/discover.js
+++
@@ -1,7 +0,0 @@
-var discover;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('discover'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
